38771d0a42db4dac3e91fc36589ee03f515ecd3c
william nordmann <william.nordmann@gmail.com>
2024-10-28 16:49:06 -0500

    Adding header and makeing popup easier to read

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     return (
         <div>
             <h1>Wait 2 Vote STL</h1>
+            <h3> Stay informed about your community's election day by exploring our interactive map, which provides real-time updates on voting locations and wait times across St Louis</h3>
             <MapComponent/>
         </div>
     );
